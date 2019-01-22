24090606badf86aba632152485119cc3c0db5c5e
zhdnf <971684859@qq.com>
2019-01-22 20:58:16 +0800

    grpc server

--- grpc/server/server.py
+++ grpc/server/server.py
@@ -15,22 +15,22 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 class Probe(systeminfo_pb2_grpc.SysinfoServicer):
     def GetSysInfo(self, request, context):
 
-        print(request.cpu*10, request.vmem*10,request.smem*10, request.disk*10, request.disk_i*10, request.disk_o*10,net_i*10, net_o*10)
-
+        print(request.cpu, request.vmem,request.smem, request.disk,
+        request.disk_i, request.disk_o,request.net_i, request.net_o)
 
         time_l = time.time()
 
         ms = mysql.Mysql()
-
         query_sql = "select * from user where ip = '%s'"
-        query_array = (request.ip,)
+        query_array = (request.ip)
+
         res = ms.query(query_sql, query_array)
 
         insert_sql = "insert into sysinfo values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
         insert_array = (res[0]['uid'], request.cpu*10, request.vmem*10,
         request.smem*10, request.disk*10, request.disk_i*10, request.disk_o*10,
-        net_i*10, net_o*10, time_l)
+        request.net_i*10, request.net_o*10, time_l)
 
         ms.insert(insert_sql, insert_array)
 
@@ -45,14 +45,14 @@ class Probe(systeminfo_pb2_grpc.SysinfoServicer):
         for i in request_iterator:
             row = {}
             row['pid'] = i.pid
-            row['name'] = i.name
-            row['cpu'] = i.cpu
-            row['mem'] = i.mem
+            row['pname'] = i.name
+            row['pcpu'] = i.cpu
+            row['pmem'] = i.mem
             ip = i.ip
 
             res.append(row)
 
-        res.sort(key = lambda x: x["mem"], reverse=True) 
+        res.sort(key = lambda x: x["pmem"], reverse=True) 
         
         ms = mysql.Mysql()
 
@@ -60,13 +60,15 @@ class Probe(systeminfo_pb2_grpc.SysinfoServicer):
         query_array = (ip,)
         res_sql = ms.query(query_sql, query_array)
 
-        insert_sql = "insert into sysinfo values(%s, %s, %s, %s, %s, %s)"
+        insert_sql = "insert into process values(%s, %s, '%s', %s, %s, %s)"
 
         for i in res[0:5]:
+            
+            print(i['pcpu'])
             insert_array = (res_sql[0]['uid'], i['pid'], i['pname'],
-            i['pcpu'],i['pmem'], time_l)
-
-            ms.insert_array(insert_sql, insert_array)
+            i['pcpu']*10, i['pmem']*10, time_l)
+            
+            ms.insert(insert_sql, insert_array)
 
         ms.close()
 
