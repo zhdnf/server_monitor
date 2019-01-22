a8cbcdd65caba0f97f436b60515662fe948e19bf
zhdnf <971684859@qq.com>
2019-01-22 20:55:50 +0800

    setting for project

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 import pymysql
 
+# mysql config
 MYSQL_CONFIG = {
     'host' : '127.0.0.1',
     'port' : 3306,
     'user' : 'root',
     'passwd' : '123',
+    'database' : 'server_monitor',
     'charset' : 'utf8mb4',
 }
+
+# gprc_server
+GPRC_IP = '192.168.203.155'
+GPRC_PORT = '50051'
