2716a69e959ce83fcde089084d9512dc28be72ac
zhdnf <971684859@qq.com>
2019-02-09 22:50:29 +0800

    sql insert script

--- utils/mysql_insert.py
+++ utils/mysql_insert.py
@@ -1,17 +1,43 @@
 import mysql
 import time 
-import random 
+import random
+from faker import Faker
 
 sql_1 = 'insert into sysinfo values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+sql_2 = "insert into process values(%s, %s, '%s', %s, %s, %s)"
 
 local_time = 1549850438
 
-mysql = mysql.Mysql()
+def cpu_insert():
+    global local_time
 
-for i in range(0,1500):
-    cpu = random.randint(0,100)
-    array = (1, cpu, 0, 0, 0, 0, 0, 0, 0, local_time)
-    mysql.insert(sql_1, array)
-    local_time = local_time + 2
-   
+    mysql1 = mysql.Mysql()
+
+    for i in range(0,1500):
+        cpu = random.randint(0,100)
+        array = (1, cpu, 0, 0, 0, 0, 0, 0, 0, local_time)
+        mysql1.insert(sql_1, array)
+        local_time = local_time + 2
+
+       
+def process_insert():
+    global local_time
+
+    mysql2 = mysql.Mysql()
+    fake = Faker()
+
+    for i in range(1800):
+
+        for j in range(5):
+            pid = random.randint(1,1000)
+            cpu = random.randint(0,100)
+            mem = random.randint(0,100) 
+            name = fake.user_name()
+            array = (1, pid, name,  cpu, mem, local_time)
+            mysql2.insert(sql_2, array)
+
+        local_time = local_time + 2
+            
+if __name__ == "__main__":
+    process_insert()
 
