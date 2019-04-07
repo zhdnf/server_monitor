5fa4b03210ea1956f561c3186571a871bed8fbcb
zhdnf <971684859@qq.com>
2019-04-07 09:07:00 +0800

    update

--- app/application.py
+++ app/application.py
@@ -1,27 +1,27 @@
 from tornado.web import Application
 
 from conf.config import TORNADO_CONFIG
-from handlers import *
+from handlers import web, user, sysinfo
 
 class App(Application):
     def __init__(self):
         self.handlers = [
-                            (r"/", MainHandler),
-                            (r"/login", LoginHandler),
-                            (r"/signup", SignUpHandler),
-                            (r"/logout", LogoutHandler),
-                            (r"/cpu", CpuPercentHandler),
-                            (r"/swap", SwapHandler),
-                            (r"/mem", MemHandler),
-                            (r"/disk", DiskHandler),
-                            (r"/disk_io", Disk_ioHandler),
-                            (r"/net_io", Net_ioHandler),
-                            (r"/process", ProcessHandler),
-                            (r"/proc", ProcHandler),
-                            (r"/404", Page404Handler)
+                            (r"/", web.MainHandler),
+                            (r"/login", user.LoginHandler),
+                            (r"/sign_up", user.RegisterHandler),
+                            (r"/logout", user.LogoutHandler),
+                            (r"/cpu", sysinfo.CpuPercentHandler),
+                            (r"/swap", sysinfo.SwapHandler),
+                            (r"/mem", sysinfo.MemHandler),
+                            (r"/disk", sysinfo.DiskHandler),
+                            (r"/disk_io", sysinfo.DiskIOHandler),
+                            (r"/net_io", sysinfo.NetIOHandler),
+                            (r"/process", sysinfo.ProcessHandler),
+                            (r"/proc", sysinfo.ProcHandler),
+                            (r"/404", web.Page404Handler)
                         ]
         self.settings = TORNADO_CONFIG
 
-        super(Application,self).__init__(self.handlers, **self.settings)
+        super(App,self).__init__(self.handlers, **self.settings)
 
 
--- app/server.py
+++ app/server.py
@@ -2,9 +2,9 @@ import sys
 sys.path.append("/root/server_monitor")
 import tornado.ioloop
 import tornado.web
-import tornado.httpserver
+from tornado import httpserver
 
-from app import App
+from application import App
 
 # 延迟时间，秒为单位
 delay = 30
@@ -16,7 +16,7 @@ interval = 60
 if __name__ == "__main__":
 
     app = App()
-    http_server = httpserver.HttpServer(app)
+    http_server = httpserver.HTTPServer(app)
     http_server.bind(7777)
     http_server.start()
     tornado.ioloop.IOLoop.current().start()
